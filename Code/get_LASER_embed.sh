4e524bb021a80a6c328fc660e63aa5d3d07efa4c
punyajoy <punyajoysaha1998@gmail.com>
2019-09-16 21:32:05 +0530

    First Commit

---
+++ Code/get_LASER_embed.sh
@@ -0,0 +1,34 @@
+export LASER="${HOME}/HASOC/LASER"
+
+# #task 1
+
+# bash ./embed.sh ../Data/english_dataset/english_data_task1_commen.txt en ../Data/english_dataset/embeddings_eng_task1_commen.raw
+# bash ./embed.sh ../Data/english_dataset/english_data_task1_lib.txt en ../Data/english_dataset/embeddings_eng_task1_lib.raw
+# bash ./embed.sh ../Data/hindi_dataset/hindi_data_task1_commen.txt en ../Data/hindi_dataset/embeddings_hin_task1_commen.raw
+# bash ./embed.sh ../Data/hindi_dataset/hindi_data_task1_lib.txt en ../Data/hindi_dataset/embeddings_hin_task1_lib.raw
+# bash ./embed.sh ../Data/german_dataset/german_data_task1_commen.txt de ../Data/german_dataset/embeddings_ger_task1_commen.raw
+# bash ./embed.sh ../Data/german_dataset/german_data_task1_lib.txt de ../Data/german_dataset/embeddings_task1_ger_lib.raw
+
+
+# #task 2 and task 3
+
+# bash ./embed.sh ../Data/english_dataset/english_data_task23_commen.txt en ../Data/english_dataset/embeddings_eng_task23_commen.raw
+# bash ./embed.sh ../Data/english_dataset/english_data_task23_lib.txt en ../Data/english_dataset/embeddings_eng_task23_lib.raw
+# bash ./embed.sh ../Data/hindi_dataset/hindi_data_task23_commen.txt en ../Data/hindi_dataset/embeddings_hin_task23_commen.raw
+# bash ./embed.sh ../Data/hindi_dataset/hindi_data_task23_lib.txt en ../Data/hindi_dataset/embeddings_hin_task23_lib.raw
+# bash ./embed.sh ../Data/german_dataset/german_data_task23_commen.txt de ../Data/german_dataset/embeddings_ger_task23_commen.raw
+# bash ./embed.sh ../Data/german_dataset/german_data_task23_lib.txt de ../Data/german_dataset/embeddings_ger_task23_lib.raw
+
+
+export LASER="${HOME}/HASOC/LASER"
+
+#test
+
+bash ./embed.sh ../Data/english_dataset/english_data_test_task1_commen.txt en ../Data/english_dataset/embeddings_eng_test_task1_commen.raw
+bash ./embed.sh ../Data/english_dataset/english_data_test_task1_lib.txt en ../Data/english_dataset/embeddings_eng_test_task1_lib.raw
+bash ./embed.sh ../Data/hindi_dataset/hindi_data_test_task1_commen.txt en ../Data/hindi_dataset/embeddings_hin_test_task1_commen.raw
+bash ./embed.sh ../Data/hindi_dataset/hindi_data_test_task1_lib.txt en ../Data/hindi_dataset/embeddings_hin_test_task1_lib.raw
+bash ./embed.sh ../Data/german_dataset/german_data_test_task1_commen.txt de ../Data/german_dataset/embeddings_ger_test_task1_commen.raw
+bash ./embed.sh ../Data/german_dataset/german_data_test_task1_lib.txt de ../Data/german_dataset/embeddings_ger_test_task1_lib.raw
+
+
